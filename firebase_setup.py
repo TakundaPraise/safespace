a2d160fec69cfd3193624e3b19265fde5dcb4843
TakundaPraise <takundapraiseg@gmail.com>
2024-11-11 11:00:19 +0200

    Update firebase_setup.py
    added an error handing file

--- firebase_setup.py
+++ firebase_setup.py
@@ -11,18 +11,18 @@ db = firestore.client()
 
 # User registration (mockup)
 def register_user(email, password):
-    user = auth.create_user(email=email, password=password)
+    user = auth.create_user(email=takundapraiseg@gmail.com, password=12345678)
     return user
 
 # Save harassment reports
-def save_report(user_id, text, label, confidence):
-        # Convert confidence to a native Python float
-    confidence = float(confidence)
-    db.collection("reports").add({
-        "user_id": user_id,
-        "text": text,
-        "label": label,
-        "confidence": confidence
-    })
-
+def save_report(user_id, user_input, label, confidence):
+    try:
+        db.collection("reports").add({
+            "user_id": user_id,
+            "input": user_input,
+            "label": label,
+            "confidence": confidence,
+        })
+    except google.api_core.exceptions.GoogleAPIError as e:
+        print(f"Error saving report to Firestore: {e}")
 
